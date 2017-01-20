276cc0abb553fe1ec0f9934b70a61cd726883b16
Kuya Takami <kuya@gwu.edu>
2017-01-19 19:57:55 -0500

    image reduction for publishing over wifi
    run time error on publishing image

---
+++ src/odroid_sense.cpp
@@ -0,0 +1,88 @@
+#include <ros/ros.h>
+#include <image_transport/image_transport.h>
+#include <sensor_msgs/image_encodings.h>
+#include <boost/thread.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <cv_bridge/cv_bridge.h>
+// #include "opencv2/core/version.hpp"
+#include <opencv/cv.h>
+// #include <opencv/highgui.h>
+
+static const char WINDOW_NAME[] = "Depth View";
+double min_range_;
+double max_range_;
+image_transport::Publisher *imagePub;
+cv::Mat resize;
+
+void imageCallback(const sensor_msgs::ImageConstPtr& msg)
+{
+  cv_bridge::CvImagePtr bridge;
+  try
+  {
+    // cv::imshow("view", cv_bridge::toCvShare(msg, "32FC1")->image);
+    // cv::waitKey(30);
+    bridge = cv_bridge::toCvCopy(msg, "32FC1");
+  }
+  catch (cv_bridge::Exception& e)
+  {
+    ROS_ERROR("Could not convert from '%s' to 'mono8'.", msg->encoding.c_str());
+  }
+
+
+
+  // cv::Mat resize;//(bridge->image.rows/2, bridge->image.cols/2, CV_8UC1);
+try{
+
+  cv::resize(bridge->image,resize, cv::Size(320.5,210.5));
+  cv_bridge::CvImage resizeRos;
+  resizeRos.encoding = "32FC1";
+  resizeRos.image = resize;
+  // republish image
+  sensor_msgs::ImagePtr imagePtr=resizeRos.toImageMsg();
+  imagePtr->width=320.5;
+  imagePtr->height=210.5;
+  // imagePub->publish(imagePtr);
+}
+catch(cv_bridge::Exception e){
+ ROS_ERROR("cv_bridge exception: %s", e.what());
+  return;
+
+}
+
+
+  // for(int i = 0; i < bridge->image.rows; i = i+2)
+  // {
+  //     float* Di = bridge->image.ptr<float>(i);
+  //     char* Ii = img.ptr<char>(i);
+  //     for(int j = 0; j < bridge->image.cols; j = j+2)
+  //     {
+  //         Ii[j/2] = (char) (255*((Di[j]-min_range_)/(max_range_-min_range_)));
+  //     }
+  // }
+
+  // display
+  cv::imshow(WINDOW_NAME, resize);
+  cv::waitKey(3);
+
+}
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "image_listener");
+  ros::NodeHandle nh;
+  // cv::namedWindow("view");
+  // cv::startWindowThread();
+
+  nh.param("min_range", min_range_, 0.5);
+  nh.param("max_range", max_range_, 5.5);
+
+  cv::namedWindow(WINDOW_NAME);
+
+  image_transport::ImageTransport it(nh);
+  // image_transport::Publisher pub = it.advertise(topic_out, 1);
+  // imagePub=&pub;
+
+  image_transport::Subscriber sub = it.subscribe("/camera/depth/image_raw", 1, imageCallback);
+  ros::spin();
+  cv::destroyWindow("view");
+}

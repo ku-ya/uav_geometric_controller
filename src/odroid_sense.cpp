93dcf0feee1829620bbcbab7974b32b03c4b00e4
Kuya Takami <kuya@gwu.edu>
2017-01-22 19:15:37 -0500

    python version of image reduction node working

--- src/odroid_sense.cpp
+++ src/odroid_sense.cpp
@@ -2,14 +2,18 @@
 #include <image_transport/image_transport.h>
 #include <sensor_msgs/image_encodings.h>
 #include <boost/thread.hpp>
-// #include <opencv2/highgui/highgui.hpp>
-// #include <opencv/cv.h>
+
 #include <cv_bridge/cv_bridge.h>
-// #include "opencv2/core/version.hpp"
-// #include <opencv/cv.h>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
-#include <opencv2/core/core.hpp>
+// #include <opencv2/core/core.hpp>
+// #if CV_MAJOR_VERSION == 2
+// // do opencv 2 code
+// #elif CV_MAJOR_VERSION == 3
+// // do opencv 3 code
+// #endif
+#include <opencv2/opencv.hpp>
+
 
 static const char WINDOW_NAME[] = "Depth View";
 double min_range_;
@@ -20,15 +24,22 @@ cv::Mat resize;
 void imageCallback(const sensor_msgs::ImageConstPtr& msg)
 {
   cv_bridge::CvImagePtr bridge;
+  sensor_msgs::ImagePtr imagePtr;
+  cv_bridge::CvImage resizeRos;
   try
   {
     // cv::imshow("view", cv_bridge::toCvShare(msg, "32FC1")->image);
     // cv::waitKey(30);
     bridge = cv_bridge::toCvCopy(msg, "32FC1");
+    cv::resize(bridge->image,resize, cv::Size(320.5,210.5));
+  resizeRos.encoding = "32FC1";
+  resizeRos.image = resize;
+
+
   }
   catch (cv_bridge::Exception& e)
   {
-    ROS_ERROR("Could not convert from '%s' to 'mono8'.", msg->encoding.c_str());
+    ROS_ERROR("Could not convert from '%s' to '32FC1'.", msg->encoding.c_str());
   }
 
 
@@ -36,14 +47,11 @@ void imageCallback(const sensor_msgs::ImageConstPtr& msg)
   // cv::Mat resize;//(bridge->image.rows/2, bridge->image.cols/2, CV_8UC1);
 try{
 
-  cv::resize(bridge->image,resize, cv::Size(320.5,210.5));
-  cv_bridge::CvImage resizeRos;
-  resizeRos.encoding = "32FC1";
-  resizeRos.image = resize;
   // republish image
-  sensor_msgs::ImagePtr imagePtr=resizeRos.toImageMsg();
+  imagePtr=resizeRos.toImageMsg();
   imagePtr->width=320.5;
   imagePtr->height=210.5;
+
   imagePub->publish(imagePtr);
 }
 catch(cv_bridge::Exception e){

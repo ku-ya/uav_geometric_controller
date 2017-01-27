bdbcd1e67653cc69ed5651d7388c42196bb1f789
Kuya Takami <kuya@gwu.edu>
2017-01-27 14:18:20 -0500

    error msg added for plotting and check

--- src/odroid_node.cpp
+++ src/odroid_node.cpp
@@ -1,6 +1,7 @@
 #include <odroid/odroid_node.hpp>
 // #include <odroid/controllers.h>// robotic control
 // User header files
+#include <odroid/error.h>
 #include <XmlRpcValue.h>
 
 using namespace std;
@@ -110,7 +111,7 @@ odroid_node::odroid_node(){
   ros::param::get("/controller/saturation/x",eiX_sat);
   ros::param::get("/controller/saturation/R",eiR_sat);
 
-  pub_ = n_.advertise<std_msgs::String>("/motor_command",1);
+  pub_ = n_.advertise<odroid::error>("/motor_command",1);
   vis_pub_0 = n_.advertise<visualization_msgs::Marker>("/force0",1);
   vis_pub_1 = n_.advertise<visualization_msgs::Marker>("/force1",1);
   vis_pub_2 = n_.advertise<visualization_msgs::Marker>("/force2",1);
@@ -277,6 +278,8 @@ void odroid_node::ctl_callback(){
   }
 
   double mean = f_motor.mean();
+  double scale = 0.1;
+
   visualization_msgs::Marker marker;
   marker.header.frame_id = "base_link";
   marker.header.stamp = vicon_time;
@@ -291,7 +294,7 @@ void odroid_node::ctl_callback(){
   marker.pose.orientation.y = -0.707;
   marker.pose.orientation.z = 0;
   marker.pose.orientation.w = 0.707;
-  marker.scale.x = f_motor(0) - mean;
+  marker.scale.x = f_motor(0) * scale;
   marker.scale.y = 0.05;
   marker.scale.z = 0.05;
   marker.color.a = 1.0; // Don't forget to set the alpha!
@@ -304,7 +307,7 @@ void odroid_node::ctl_callback(){
 
   marker.pose.position.x = 0;
   marker.pose.position.y = -0.3;
-  marker.scale.x = f_motor(1) - mean;
+  marker.scale.x = f_motor(1)* scale;
   marker.color.r = 0.0;
   marker.color.g = 1.0;
   marker.color.b = 0.0;
@@ -313,7 +316,7 @@ void odroid_node::ctl_callback(){
 
   marker.pose.position.x = -0.3;
   marker.pose.position.y = 0;
-  marker.scale.x = f_motor(2) - mean;
+  marker.scale.x = f_motor(2)* scale;
   marker.color.r = 0.0;
   marker.color.g = 0.0;
   marker.color.b = 1.0;
@@ -322,7 +325,7 @@ void odroid_node::ctl_callback(){
 
   marker.pose.position.x = 0;
   marker.pose.position.y = 0.3;
-  marker.scale.x = f_motor(3) - mean;
+  marker.scale.x = f_motor(3)* scale;
   marker.color.r = 0.0;
   marker.color.g = 0.0;
   marker.color.b = 1.0;
@@ -458,6 +461,18 @@ void odroid_node::QuadGeometricPositionController(Vector3d xd, Vector3d xd_dot, 
 
 
     f_motor = Ainv*FM;
+
+    odroid::error e_msg;
+    Vector3d kR_eR = kR*eR;
+    Vector3d kW_eW = kW*eW;
+    e_msg.kW = kW; e_msg.kR = kR;
+    e_msg.eR.x = eR(0); e_msg.eR.y = eR(1);e_msg.eR.z = eR(2);
+    e_msg.kR_eR.x = kR_eR(0); e_msg.kR_eR.y = kR_eR(1);e_msg.kR_eR.z = kR_eR(2);
+    e_msg.eW.x = eW(0); e_msg.eW.y = eW(1);e_msg.eW.z = eW(2);
+    e_msg.kW_eW.x = kW_eW(0); e_msg.kW_eW.y = kW_eW(1);e_msg.kW_eW.z = kW_eW(2);
+    e_msg.M.x = M(0); e_msg.M.y = M(1);e_msg.M.z = M(2);
+    pub_.publish(e_msg);
+
 }
 
 void odroid_node::gazebo_controll(){

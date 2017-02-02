58aa76ba73494c328956f8344fbe5936cdbcd0d4
Kuya Takami <kuya@gwu.edu>
2017-02-02 10:19:22 -0500

    data file generation complete

--- scripts/rgbd_laser_subscriber.py
+++ scripts/rgbd_laser_subscriber.py
@@ -8,6 +8,7 @@ import message_filters
 from std_msgs.msg import String
 from sensor_msgs.msg import Image, LaserScan
 from cv_bridge import CvBridge, CvBridgeError
+import numpy as np
 
 # from __future__ import print_function
 class image_converter:
@@ -30,18 +31,26 @@ class image_converter:
 
     (rows,cols) = cv_image.shape
     # print(rows, cols)
-    image[235:245,:,0] = 0
-    image[235:245,:,1] = 0
-    image[235:245,:,2] = cv_image[235:245,:]
+    # image[235:245,:,0] = 0
+    # image[235:245,:,1] = 0
+    # image[235:245,:,2] = cv_image[235:245,:]
 
     if cols > 60 and rows > 60 :
       cv2.circle(cv_image, (50,50), 10, 255)
 
-    cv_image = cv2.resize(cv_image, (0,0), fx=0.2, fy=0.2)
+    # cv_image = cv2.resize(cv_image, (0,0), fx=0.2, fy=0.2)
 
     # cv2.imshow("Image window", cv_image)
-    cv2.imshow("Image window", image)
-    cv2.waitKey(10)
+    # cv2.imshow("Image window", image)
+    # cv2.waitKey(10)
+
+    try:
+      fname =str(depth_data.header.stamp)
+      cv2.imwrite('rgbd/'+fname+'.jpg',image[240:241,:,:])
+      np.save('depth/'+fname, cv_image[240,:])
+      np.save('laser/'+fname, laser_data.ranges)
+    except CvBridgeError as e:
+      print(e)
 
     try:
       self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "passthrough"))

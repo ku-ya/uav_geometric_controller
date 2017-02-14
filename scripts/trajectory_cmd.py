c5f5014e4edc36ff71a6b86fe05b6fb369844ba4
Evan Kaufman GWU iMac <evantkaufman@gmail.com>
2017-02-14 14:45:59 -0500

    changed all cmds with ROS time, needs testing

--- scripts/trajectory_cmd.py
+++ scripts/trajectory_cmd.py
@@ -14,6 +14,7 @@ def char_to_mission(argument):
         'f': 'figure8',
         'e': 'lissajous',
         'p': 'p2p',
+        'r': 'reset'
         'h': 'hover',
         'm': 'motor',
         'w': 'warmup'
@@ -75,6 +76,10 @@ def cmd(msg):
         N = round(t_total / dt)
         pi_speed = np.pi * 2 / N
         speed = 1./N
+        reset_height = 1.5
+        min_flight_height = 0.2
+        vel_up = 0.5
+        vel_dn = -0.5
         
 
         print mission
@@ -89,95 +94,100 @@ def cmd(msg):
 
             # t = (t_last - t_init)*speed
             x = y = z = x_dot = y_dot = z_dot = 0
-            t = speed * loop
-            print t
+            a = 2*np.pi/t_total# maps t = 0:t_total to 0:2*pi
+            b = 2*a# maps t = 0:t_total to 0:4*pi (Lissajous figure 8)
+#            t = speed * loop
+#            print t
 
-            if mission=='figure8':
-                t = pi_speed * loop + np.pi/2
-                a = 2.0
-                x = a*np.cos(t)/(1+np.sin(t)**2)
-                y = a*np.sin(t)*np.cos(t)/(1+np.sin(t)**2)
-                z = 1.5 # 2 + np.sin(t*np.pi/2)
-                x_dot = - a*np.sin(t)/(np.sin(t)**2 + 1) - 2*a*np.sin(t)*np.cos(t)**2/(np.sin(t)**2+1)**2
-                y_dot = - a*np.sin(t)**2/(np.sin(t)**2 + 1) + a*np.cos(t)**2/(np.sin(t)**2+1) - 2*a*np.sin(t)**2*np.cos(t)**2/(np.sin(t)**2+1)**2
-                z_dot = 0
-                if loop > N:
-                    print('Lemniscate figure 8 complete')
-                    break
-            elif mission=='lissajous':
+#            if mission=='figure8':
+#                t = pi_speed * loop + np.pi/2
+#                a = 2.0
+#                x = a*np.cos(t)/(1+np.sin(t)**2)
+#                y = a*np.sin(t)*np.cos(t)/(1+np.sin(t)**2)
+#                z = 1.5 # 2 + np.sin(t*np.pi/2)
+#                x_dot = - a*np.sin(t)/(np.sin(t)**2 + 1) - 2*a*np.sin(t)*np.cos(t)**2/(np.sin(t)**2+1)**2
+#                y_dot = - a*np.sin(t)**2/(np.sin(t)**2 + 1) + a*np.cos(t)**2/(np.sin(t)**2+1) - 2*a*np.sin(t)**2*np.cos(t)**2/(np.sin(t)**2+1)**2
+#                z_dot = 0
+#                if loop > N:
+#                    print('Lemniscate figure 8 complete')
+#                    break
+#            el
+            if mission=='lissajous':
                 t_now = (rospy.get_rostime()).to_sec()
                 t = t_now-t_init
-                a = pi_speed
-                b = 2*pi_speed
-                start_rad_x = np.pi/2
-                start_rad_y = b/a*start_rad_x
+
+                start_radians_x = np.pi/2
+                start_radians_y = b/a*start_radians_x
                 A = 2.0
                 B = 1.0
                 delta = np.pi/2
-                x = A*np.sin(a*t+delta+start_rad_x)
-                x_dot = a*A*np.cos(a*t*delta+start_rad_x)
-                x_ddot = -a**2*A*np.sin(a*t*delta+start_rad_x)
-                y = B*np.sin(b*t+start_rad_y)
-                y_dot = b*B*np.cos(b*t+start_rad_y)
-                y_ddot = -b**2*B*np.sin(b*t+start_rad_y)
-                z = 1.5
+                x = A*np.sin(a*t+delta+start_radians_x)
+                x_dot = a*A*np.cos(a*t*delta+start_radians_x)
+                x_ddot = -a**2*A*np.sin(a*t*delta+start_radians_x)
+                y = B*np.sin(b*t+start_radians_y)
+                y_dot = b*B*np.cos(b*t+start_radians_y)
+                y_ddot = -b**2*B*np.sin(b*t+start_radians_y)
+                z = reset_height
                 z_dot = 0.0
                 z_ddot = 0.0
 
-                if loop > N:
+                if t > t_total:
                     print('Lissajous figure 8 complete')
                     break
             elif mission=='p2p':
-                t = pi_speed * loop
-                x = np.sin(t)
-                y = np.sin(t)
-                z = 1.5 + np.sin(t)/2
-                x_dot = np.cos(t)
-                y_dot = np.cos(t)
-                z_dot = np.cos(t)/2
-                x_ddot = -np.sin(t)
-                y_ddot = -np.sin(t)
-                z_ddot = -np.sin(t)/2
+                t_now = (rospy.get_rostime()).to_sec()
+                t = t_now-t_init
+                x = np.sin(a*t)
+                y = np.sin(a*t)
+                z = reset_height+np.sin(a*t)/2
+                x_dot = np.cos(t)*a
+                y_dot = np.cos(t)*a
+                z_dot = (np.cos(t)/2)*a
+                x_ddot = -np.sin(t)*a**2
+                y_ddot = -np.sin(t)*a**2
+                z_ddot = -(np.sin(t)/2)*a**2
 
-                if loop > N:
+                if t > t_total:
                     print('p2p complete')
                     break
             elif mission == 'takeoff':
+                t_now = (rospy.get_rostime()).to_sec()
+                t = t_now-t_init
                 x = 0
                 y = 0
-                z = 0.2 + 1.5*speed*loop if 1.5*speed*loop < 1.5 else 1.5
+                z = min_flight_height+vel_up*t if min_flight_height+vel_up*t < 1.5 else 1.5
                 x_dot = 0
                 y_dot = 0
-                z_dot = 0
-                if loop > N:
+                z_dot = vel_up
+                if t > t_total:
                     print('takeoff complete')
                     break
-            elif mission == 'hover':
+            elif mission == 'reset':
                 x = 0
                 y = 0
-                z = 1.5
+                z = reset_height
                 x_dot = 0
                 y_dot = 0
                 z_dot = 0
                 x_ddot = 0
                 y_ddot = 0
                 z_ddot = 0
-                if loop > N:
-                    print('hover complete')
-                    break
+
+                print('resetting...')
+                break
             elif mission == 'land':
                 t_now = (rospy.get_rostime()).to_sec()
                 t = t_now-t_init
                 x = 0
                 y = 0
-                z = 1.5 - t*0.5 if 1.5 - t*0.5 > 0.2 else 0
+                z = reset_height+t*vel_dn if reset_height+t*vel_dn > min_flight_height else 0
                 x_dot = 0
                 y_dot = 0
-                z_dot = - 0.5
+                z_dot = vel_dn
                 x_ddot = 0
                 y_ddot = 0
                 z_ddot = 0
-                if z <= 0.2:
+                if z <= min_flight_height:
                     rospy.set_param('/odroid_node/Motor', False)
                     pub.publish(cmd)
                     print('landing complete')
@@ -187,23 +197,13 @@ def cmd(msg):
             cmd.xd.y = y
             cmd.xd.z = z
 
-            cmd.xd_dot.x = speed * x_dot
-            cmd.xd_dot.y = speed * y_dot
-            cmd.xd_dot.z = speed * z_dot
-            cmd.xd_ddot.x = 0#speed**2 * x_ddot
-            cmd.xd_ddot.y = 0#speed**2 * y_ddot
-            cmd.xd_ddot.z = 0#speed**2 * z_ddot
+            cmd.xd_dot.x = x_dot
+            cmd.xd_dot.y = y_dot
+            cmd.xd_dot.z = z_dot
 
-            if mission == 'lissajous' or mission == "land":
-                cmd.xd_dot.x = x_dot
-                cmd.xd_dot.y = y_dot
-                cmd.xd_dot.z = z_dot
-
-                cmd.xd_ddot.x = x_ddot
-                cmd.xd_ddot.y = y_ddot
-                cmd.xd_ddot.z = z_ddot
-
-
+            cmd.xd_ddot.x = x_ddot
+            cmd.xd_ddot.y = y_ddot
+            cmd.xd_ddot.z = z_ddot
 
             loop = loop + 1.0
             pub.publish(cmd)

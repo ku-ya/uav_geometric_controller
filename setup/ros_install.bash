57a3abcc95710f50b7b82c4231fc086a5c9fcb5d
Shankar Kulumani SEAS-Linux <skulumani@gwmail.gwu.edu>
2017-02-14 17:06:02 -0500

    Update ROS install script

--- setup/ros_install.bash
+++ setup/ros_install.bash
@@ -1,14 +1,25 @@
 #!/bin/bash
 # time synchronization
+echo "Starting to install ROS and associated stuff"
 sudo apt-get install chrony
 sudo apt-get install ntpdate
 
 # ROS install
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
-sudo apt-get update
-sudo apt-get install ros-kinetic-ros-base
+sudo apt-get -y update
+sudo apt-get -y install ros-kinetic-desktop-full
 sudo rosdep init
 rosdep update
 echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
 source ~/.bashrc
+
+echo "Install Python related stuff"
+sudo apt-get -y install python-rosinstall
+
+sudo apt-get -y install python-pip python3-pip
+pip3 install --user rospkg catkin_pkg
+pip install --user numpy scipy matplotlib ipython jupyter pandas sympy nose
+
+echo "Adding local Python to path"
+export PATH="$PATH:$HOME/.local/bin"

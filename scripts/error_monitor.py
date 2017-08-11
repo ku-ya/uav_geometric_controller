5d950000d8b691fb20be6a6164902c05daae263a
Kuya Takami <kuya@gwu.edu>
2017-08-11 13:52:36 -0400

    error monitor plot update

--- scripts/error_monitor.py
+++ scripts/error_monitor.py
@@ -7,27 +7,43 @@ import matplotlib.pyplot as plt
 import time
 import threading
 import random
+from collections import deque
 
-data = []
+eW = deque([0]*10, 100)
 
 # This just simulates reading from a socket.
-def data_listener():
-    while True:
-        time.sleep(1)
-        data.append(random.random())
+def callback(data):
+
+    pass
 
 if __name__ == '__main__':
-    thread = threading.Thread(target=data_listener)
-    thread.daemon = True
-    thread.start()
+    rospy.init_node('error_plot', anonymous=True)
+    rospy.Subscriber("err", trajectory, callback)
+
+    # thread = threading.Thread(target=data_listener)
+    # thread.daemon = True
+    # thread.start()
+    # time.sleep(1)
     #
     # initialize figure
-    plt.figure()
-    ln, = plt.plot([])
+    fig = plt.figure()
+    ax_eW = plt.subplot(311)
+    ax_eR = plt.subplot(312)
+    ax_M = plt.subplot(313)
+    ln, = ax_eR.plot([],'rs')
     plt.ion()
     plt.show()
     while True:
-        plt.pause(1)
-        ln.set_xdata(range(len(data)))
-        ln.set_ydata(data)
+        plt.pause(0.1)
+        # ln.set_xdata(range(len(data)))
+        # ln.set_ydata(data)
+        ax_eW.cla()
+        ax_eW.set_ylabel('eW')
+        ax_eW.plot(eW)
+        ax_eR.cla()
+        ax_eR.set_ylabel('eR')
+        ax_eR.plot(eR)
+        ax_M.cla()
+        ax_M.set_ylabel('M')
+        ax_M.plot(M)
         plt.draw()

251108f9b36568ee188fd2ffb4d34f13b8c5ea8a
Kuya Takami <kuya@gwu.edu>
2017-08-25 09:16:16 -0400

    variable name change

--- scripts/monitor.py
+++ scripts/monitor.py
@@ -29,25 +29,25 @@ class DaqThread(Thread):
     def run(self):
         print(self.args)
         while not self.wants_abort:
-            print('Starting...')
+            print('Starting... ' + self.args)
             os.system(self.args)
             self.wants_abort = True
-            print('Process...killed')
+            print('Process:' + self.args+ ' killed')
             sleep(0.1)
             pass
 
 
 class Viewer(HasTraits):
     index = Array(dtype=np.float64, shape=(None))
-    data = Array(dtype=np.float64, shape=(None))
-    data_y = Array(dtype=np.float64, shape=(None))
-    data_z = Array(dtype=np.float64, shape=(None))
+    eW_data_x = Array(dtype=np.float64, shape=(None))
+    eW_data_y = Array(dtype=np.float64, shape=(None))
+    eW_data_z = Array(dtype=np.float64, shape=(None))
 
-    eR_data = Array(dtype=np.float64, shape=(None))
+    eR_data_x = Array(dtype=np.float64, shape=(None))
     eR_data_y = Array(dtype=np.float64, shape=(None))
     eR_data_z = Array(dtype=np.float64, shape=(None))
 
-    M_data = Array(dtype=np.float64, shape=(None))
+    M_data_x = Array(dtype=np.float64, shape=(None))
     M_data_y = Array(dtype=np.float64, shape=(None))
     M_data_z = Array(dtype=np.float64, shape=(None))
 
@@ -64,21 +64,21 @@ class Viewer(HasTraits):
 
     def __init__(self, *args, **kw):
         super(Viewer, self).__init__(*args, **kw)
-        plotdata = ArrayPlotData(time=self.index, x=self.data)
+        plotdata = ArrayPlotData(time=self.index, x=self.eW_data_x)
         plot = Plot(plotdata)
         plot.plot(('time', 'x'), type="scatter", color="blue")
         self.plot_eW = plot
         self.plot_eR = plot
         self.plot_M = plot
 
-    def _data_changed(self):
-        self.plot_eW = self.plot_default(self.index, self.data, self.data_y, self.data_z)
+    def _eW_data_x_changed(self):
+        self.plot_eW = self.plot_default(self.index, self.eW_data_x, self.eW_data_y, self.eW_data_z)
 
-    def _eR_data_changed(self):
-        self.plot_eR = self.plot_default(self.index, self.eR_data, self.eR_data_y, self.eR_data_z)
+    def _eR_data_x_changed(self):
+        self.plot_eR = self.plot_default(self.index, self.eR_data_x, self.eR_data_y, self.eR_data_z)
 
-    def _M_data_changed(self):
-        self.plot_M = self.plot_default(self.index, self.M_data, self.M_data_y, self.M_data_z)
+    def _M_data_x_changed(self):
+        self.plot_M = self.plot_default(self.index, self.M_data_x, self.M_data_y, self.M_data_z)
 
     def plot_default(self, time, x, y, z):
         plotdata = ArrayPlotData(time=time, x=x, y=y, z=z)
@@ -92,18 +92,25 @@ class ErrorView(HasTraits):
     name = Str
     error_val = Enum('eW', 'eR', 'M')
     N = Int(200)
-    eW = np.zeros((200,3))#deque([0]*100, 100)
+    eW = np.zeros((200,3))
     eR = np.zeros((200,3))
     M = np.zeros((200,3))
     time = deque([0]*200, 200)
     start_stop_motor = Button()
     rqt_reconfig = Button()
+    # TODO: add ssh script exe on uav
+    # linux terminal ssh and run script
+    controller = Button()
+    mapping = Button()
+    exploration = Button()
+    openGL = Button()
     mean = Float(0.0)
     stddev = Float(1.0)
     max_num_points = Int(100)
     num_ticks = Int(0)
 
-    mission = Enum('takeoff','land')
+    mission = Enum('takeoff','land','spin')
+    mission_exe = Button()
 
     capture_thread = Instance(DaqThread)
     rqt_thread = Instance(DaqThread)
@@ -113,11 +120,12 @@ class ErrorView(HasTraits):
         HGroup(
         Item('start_stop_motor', label='start stop motor', show_label=False),
         Item('rqt_reconfig', label='Gain tuning', show_label=False),
-        Item('rqt_reconfig', label='Controller', show_label=False),
-        Item('rqt_reconfig', label='Mapping', show_label=False),
-        Item('rqt_reconfig', label='OpenGL', show_label=False),
-        Item('rqt_reconfig', label='Exporation', show_label=False),
+        Item('controller', label='Controller', show_label=False),
+        Item('mapping', label='Mapping', show_label=False),
+        Item('openGL', label='Visualization', show_label=False),
+        Item('exploration', label='Exploration', show_label=False),
         Item('mission', label='mission', show_label=False),
+        Item('mission_exe', label='Run mission', show_label=False),
         label='Run',
         ),
         Group(
@@ -141,6 +149,9 @@ class ErrorView(HasTraits):
         )
 
     def _N_changed(self):
+        """
+        Set number of data sample to plot
+        """
         self.eW = np.zeros((self.N,3))
         self.eR = np.zeros((self.N,3))
         self.M = np.zeros((self.N,3))
@@ -149,7 +160,9 @@ class ErrorView(HasTraits):
         logging.debug('errer val changed to ' + self.error_val)
 
     def _start_stop_motor_fired(self):
-        logging.debug('errer val changed')
+        """
+        TODO
+        """
         if self.capture_thread and self.capture_thread.isAlive():
             self.capture_thread.wants_abort = True
         else:
@@ -159,7 +172,9 @@ class ErrorView(HasTraits):
         pass
 
     def _rqt_reconfig_fired(self):
-        logging.debug('errer val changed')
+        """
+        Runs ros rqt_reconfigure
+        """
         if self.rqt_thread and self.rqt_thread.isAlive():
             self.rqt_thread.wants_abort = True
         else:
@@ -178,43 +193,53 @@ class ErrorView(HasTraits):
         self.time = np.linspace(0, 0.01*len(error_data), len(error_data))
 
         self.viewer.index = list(self.time)
-        self.viewer.data = list(error_data[:,0])
-        self.viewer.data_y = list(error_data[:,1])
-        self.viewer.data_z = list(error_data[:,2])
+        self.viewer.eW_data_x = list(error_data[:,0])
+        self.viewer.eW_data_y = list(error_data[:,1])
+        self.viewer.eW_data_z = list(error_data[:,2])
 
         self.time = np.linspace(0, 1, len(self.eR))
 
-        self.viewer.eR_data = list(self.eR[:,0])
+        self.viewer.eR_data_x = list(self.eR[:,0])
         self.viewer.eR_data_y = list(self.eR[:,1])
         self.viewer.eR_data_z = list(self.eR[:,2])
 
-        self.viewer.M_data = list(self.M[:,0])
+        self.viewer.M_data_x = list(self.M[:,0])
         self.viewer.M_data_y = list(self.M[:,1])
         self.viewer.M_data_z = list(self.M[:,2])
         return
 
+
 class main(HasTraits):
+    """
+    main window and viewer handler
+    """
     error_window = Instance(ErrorView)
     viewer = Instance(Viewer, ())
     timer = Instance(Timer)
     view = View(Item('error_window', style='custom', show_label=False),
                 Item('viewer', style='custom', show_label=False),
                 resizable=True)
+
     def edit_traits(self, *args, **kws):
-        # Start up the timer! We should do this only when the demo actually
-        # starts and not when the demo object is created.
+        # Start up the timer! We should do this only when the main actually
+        # starts and not when the main object is created.
         self.timer=Timer(10, self.error_window.timer_tick)
         return super(main, self).edit_traits(*args, **kws)
+
     def configure_traits(self, *args, **kws):
-        # Start up the timer! We should do this only when the demo actually
-        # starts and not when the demo object is created.
         self.timer=Timer(10, self.error_window.timer_tick)
         return super(main, self).configure_traits(*args, **kws)
+
     def _error_window_default(self):
         return ErrorView(viewer=self.viewer)
 
     def ros_callback(self, data):
-        # pass eR eW M
+        """
+        Callback for getting all the UAV state variables
+        eR
+        eW
+        M
+        """
         self.error_window.eW = np.vstack((self.error_window.eW[1:,:],
             np.array([data.eW.x, data.eW.y, data.eW.z])))
         self.error_window.eR = np.vstack((self.error_window.eR[1:,:],

79202069a231d0d2e694f1a729e209f0e0e499ad
Kuya Takami <kuya@gwu.edu>
2017-09-20 20:19:32 -0400

    estimator: threading the prediction

--- scripts/estimator.py
+++ scripts/estimator.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 import numpy as np
 from multiprocessing import Process
-from threading import Thread
+from threading import Thread, Timer, Event
 from numpy.linalg import norm
 import time
 import pprint
 import sys
+import sched
 
 # ROS packages
 import rospy
@@ -19,6 +20,7 @@ class Estimator(object):
     Estimator node class for ROS
     """
     def __init__(self, uav_name = 'UAV', num_state = None, num_sens = None):
+        self.s = sched.scheduler(time.time, time.sleep)
         # ROS node setup: subscriber and publisher
         self.nh = rospy.init_node('Estimator', anonymous=False)
         self.pose_pub = rospy.Publisher('pose_est', PoseStamped, queue_size=1)
@@ -37,32 +39,58 @@ class Estimator(object):
         self.R = np.matrix(np.identity(num_sens)*R_std**2)
         self.I = np.matrix(np.identity(num_state))
 
+        self.timer = None
+        self.dt = 0.5
+        self.next_predict = time.time()
+
+        self.start()
+        time.sleep(3)
+        self.cancel()
+
         try:
-            rospy.spin()
+            # rospy.spin()
+            pass
         except KeyboardInterrupt:
             print "Exiting..."
         finally:
             print "\nKilling estimator node"
         pass
 
+
+    def start(self):
+        print "Prediction thread started..."
+        self.timer_stop = Event()
+        self.timer = Thread(target=self.thread_for_pub)
+        self.timer.daemon = True
+        self.timer.start()
+
+    def cancel(self):
+        self.timer_stop.set()
+
     def vicon_callback(self, data):
         # TODO update attitude
+        # next_call reset to current time + dt
+        self.next_predict = time.time() + self.dt
         self.pose_msg = data
         self.state_update(self.vec2npVec(data.pose.position))
 
     def imu_callback(self, data):
-        self.imu_msg.angular_velocity = data.angular_velocity
-        self.imu_msg.linear_acceleration = data.linear_acceleration
+        self.imu_msg = data
+        # self.imu_msg.angular_velocity = data.angular_velocity
+        # self.imu_msg.linear_acceleration = data.linear_acceleration
         # TODO lock
 
-    def thread_for_pub():
-        # self.state_update()
-        #self.state_predict()
-
+    def thread_for_pub(self):
         # publish all the data
         # TODO update the 1 to 1 to filter
-        self.imu_pub.publish(data)
-        self.pose_pub.publish(self.pose_msg)
+        self.next_predict = time.time()
+        while not self.timer_stop.is_set():
+            print self.next_predict
+            self.next_predict = self.next_predict+ self.dt;
+            time.sleep(self.next_predict - time.time())
+            self.state_predict()
+            self.imu_pub.publish(self.imu_msg)
+            self.pose_pub.publish(self.pose_msg)
 
     def state_predict(self):
         self.x = self.F*self.x
